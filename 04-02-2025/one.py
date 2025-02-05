3311690f02a27563ec4407b2cc4d4fc24b4abd12
kavitha <kavinu2010@gmail.com>
2025-02-05 10:00:12 +0100

    area of rectangle

--- 04-02-2025/one.py
+++ 04-02-2025/one.py
@@ -1,6 +1,19 @@
-length, bredth = input('enter length and bredth of the rectangle').split()
-print(f'length: {length}, bredth: {bredth}')
-len=int(length)
-bre=int(bredth)
-area=len*bre
-print(f'area of rectangle is >> {area}')
+def rectangle():
+  length, bredth = input('enter length and bredth of the rectangle').split()
+  print(f'length: {length}, bredth: {bredth}')
+  
+  if length.isdigit() and bredth.isdigit():
+    len=int(length)
+    bre=int(bredth)
+    area=len*bre
+    print(f'area of rectangle is >> {area}')
+  else:
+    print('type valid number')
+
+
+def main(): 
+ rectangle()
+
+main()
+
+  
