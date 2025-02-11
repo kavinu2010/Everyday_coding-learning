7093140699291ca50ab9ef9cdef5b70626ee595b
kavitha <kavinu2010@gmail.com>
2025-02-11 22:54:35 +0100

    using real current data

--- 11-02-2025/digital_clock.c
+++ 11-02-2025/digital_clock.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+/* #include<stdio.h>
 #include<time.h>
 #include<unistd.h>
 #include<stdlib.h>
@@ -32,4 +32,18 @@ int main()
   sleep(1);
   }
   return (0);
+}*/
+
+#include<stdio.h>
+#include<time.h>
+
+int main(){
+  time_t s, val = 1;
+  struct tm* current_time;
+
+  s = time(NULL);
+  current_time = localtime(&s);
+
+  printf("%02d : %02d : %02d", current_time->tm_hour, current_time->tm_min, current_time->tm_sec);
+  return 0;
 }
