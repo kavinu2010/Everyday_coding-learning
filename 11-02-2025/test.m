ea17fda6061c0ca740b5bc828e4b3934b159185d
kavitha <kavinu2010@gmail.com>
2025-02-13 21:56:29 +0100

    while

--- 11-02-2025/test.m
+++ 11-02-2025/test.m
@@ -2,6 +2,7 @@ disp("Hello from MATLAB in VS Code!");
 
 
 %legend, axis, linspace, hold on, grid on, shapes, matrix, doc plöt. help plot
-%title. x-label, y-label, sub-plot, anonymous @, if condition
+%title. x-label, y-label, sub-plot, anonymous @, if condition 
+%while loop , %% sections, section run, tic toc, time_if, time_for
 
 10<5
